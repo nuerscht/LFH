fd4aa64d81557232b2fb522a5666fc57675af006
Sandro Dallo <sandro@dallo.biz>
2013-11-19 13:52:56 +0100

    Api Controller hinzugefügt -> Nur mal ausprobiert wie es funzt! @Andy tu der kenn zwang ah zums wieder abändere :-)

--- app/controllers/Api.java
+++ app/controllers/Api.java
@@ -6,7 +6,8 @@ import play.mvc.Result;
 public class Api extends Controller {
 	
 	public static Result customers(String id, String datetime){
-		String type = request().getQueryString("type"); // Is necessary because type is valid scala type and hence the route definition causes a compilation error
+		 // Is necessary because type is valid scala type and hence the route definition causes a compilation error
+		String type = request().getQueryString("type");
 		type = type == null ? "xml" : type;
 
 		if(datetime == null)
