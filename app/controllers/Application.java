cc3bee8579b5d84bb92a6d4f19261815932c6f63
Patrick Bösch <patrick.boesch@vision.ch>
2013-12-02 16:45:44 +0100

    Registration Login Logout Profilansicht Profil bearbeiten Transaktionsübersicht
    implementiert. Fehlt noch UnitTests und Kommentare

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,23 +1,17 @@
 package controllers;
 
-import models.*;
-import play.*;
 import play.mvc.*;
-import com.avaje.ebean.Ebean;
-import com.avaje.ebean.SqlRow;
-import com.avaje.ebean.config.GlobalProperties;
 
 import views.html.*;
 
-public class Application extends Controller {
+public class Application extends Eshomo {
 
     public static Result index() {
-        User user = User.find.byId(1);
+    	models.User user = models.User.find.byId(1);
         if (user != null) {
-            return ok(index.render(user.getEmail() + ": " + user.getType().getDescription()));
+            return ok(index.render(user.getEmail() + ": " + user.getType().getDescription(), getLoginContent()));
         } else {
-            return ok(index.render("Welcome to the LFH shop"));
+            return ok(index.render("Welcome to the LFH shop", getLoginContent()));
         }
     }
-
 }
