58c1c725cc0600c5b2b4ee7a9df01e4482860c14
Andy Villiger <a.villiger@gmail.com>
2013-12-03 22:30:41 +0100

    yaml database changes: divide test and initial data

--- app/Global.java
+++ app/Global.java
@@ -15,22 +15,37 @@ public class Global extends GlobalSettings {
 	 */
 	public void  onStart(Application app){
 		// Load initial data to the database
-		loadInitData(app);		
+		loadInitData(app);
+
+        // Load test data to the database
+        loadTestData(app);
 	}
 
 	private void loadInitData(Application app) {
-		if(Ebean.find(Product.class).findRowCount() == 0){			
+		if(Ebean.find(CartStatus.class).findRowCount() == 0){
 			@SuppressWarnings("unchecked")
-			Map<String,List<Object>> data = (Map<String,List<Object>>)Yaml.load("initial-data.yml");
-			Ebean.save(data.get("products"));
-			Ebean.save(data.get("attributes"));
-			Ebean.save(data.get("tags"));
-			Ebean.save(data.get("prodTags"));
+			Map<String,List<Object>> data = (Map<String,List<Object>>)Yaml.load("data-initial.yml");
+            Ebean.save(data.get("cartstatus"));
 			Ebean.save(data.get("usertypes"));
 			Ebean.save(data.get("users"));
-			Ebean.save(data.get("adresses"));
+			Ebean.save(data.get("addresses"));
 		}
 		
 	}
+
+
+    private void loadTestData(Application app) {
+        if(Ebean.find(Product.class).findRowCount() == 0){
+            @SuppressWarnings("unchecked")
+            Map<String,List<Object>> data = (Map<String,List<Object>>)Yaml.load("data-test.yml");
+            Ebean.save(data.get("products"));
+            Ebean.save(data.get("attributes"));
+            Ebean.save(data.get("tags"));
+            Ebean.save(data.get("prodTags"));
+            Ebean.save(data.get("users"));
+            Ebean.save(data.get("addresses"));
+        }
+
+    }
 
 }
